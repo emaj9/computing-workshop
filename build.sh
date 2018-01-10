94b2b1dfdfe3c4c12e293f9dff5173e2282a6d28
Eric Mayhew <ejmayhew@gmail.com>
2018-01-10 17:11:28 -0500

    here we go the pdfs will work c'mon baby

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ make
 mv -vt cw-website/pdf \
     lesson-plans/0/0-lp.pdf \
     lesson-plans/1/1-lp.pdf \
-    lesson-plans/syllabus.pdf
+    lesson-plans/syllabus/syllabus.pdf
 stack build
 (cd cw-website ; stack exec cw-website-exe rebuild)
 (cd cw-backend ; stack install)
