fd0ef2d285c286590523844f2e3da8181cbd6f88
Jacob Errington <git@mail.jerrington.me>
2018-01-13 15:47:23 -0500

    adjust build script
    The script now copies PDFs from lessons/ as well as from the extra-pdf
    directory

--- build.sh
+++ build.sh
@@ -2,8 +2,15 @@
 
 set -e
 
+function copy-pdfs() {
+    find "$1" -name '*.pdf' -exec cp -v {} cw-website/pdf \;
+}
+
+mkdir -p extra-pdf
+
 make
-find lessons -name '*.pdf' -exec cp -v {} cw-website/pdf \;
+copy-pdfs lessons
+copy-pdfs extra-pdf
 stack build
 (cd cw-website ; stack exec cw-website-exe rebuild)
 (cd cw-backend ; stack install)
