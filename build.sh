094118a13afac850922fe00a2609b7ce30521cc3
Jacob Errington <git@mail.jerrington.me>
2018-01-13 15:52:23 -0500

    revamp ops

--- build.sh
+++ build.sh
@@ -2,15 +2,29 @@
 
 set -e
 
+LOG=/tmp/cw-build.log
+
 function copy-pdfs() {
     find "$1" -name '*.pdf' -exec cp -v {} cw-website/pdf \;
 }
 
+function uh-oh() {
+    cat /tmp/cw-build.log
+    exit 1
+}
+
 mkdir -p extra-pdf
 
-make
-copy-pdfs lessons
-copy-pdfs extra-pdf
-stack build
-(cd cw-website ; stack exec cw-website-exe rebuild)
-(cd cw-backend ; stack install)
+echo ">>> BUILDING PDFs"
+make > "$LOG" || uh-oh
+
+echo ">>> COPYING PDFs"
+copy-pdfs lessons > "$LOG" 2>&1 || uh-oh
+copy-pdfs extra-pdf > "$LOG" 2>&1 || uh-oh
+
+echo ">>> BUILDING HASKELL APPICATIONS"
+stack build > "$LOG" || uh-oh
+
+echo ">>> BUILDING FRONTEND"
+(cd cw-website ; stack exec cw-website-exe rebuild) > "$LOG" 2>&1 || uh-oh
+(cd cw-backend ; stack install) > "$LOG" 2>&1 || uh-oh
--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,8 @@
 
 set -e
 
+echo ">>> DEPLOYING STATIC CONTENT"
 rsync -r cw-website/_site/ /srv/http/computing-workshop/
+
+echo ">>> RESTARTING BACKEND"
 sudo systemctl restart cw-backend
