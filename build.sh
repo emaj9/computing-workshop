c5940ab523e4b5c71a47917d4384df0d771769fc
Jacob Errington <git@mail.jerrington.me>
2018-01-13 15:35:45 -0500

    use find to deploy all PDFs
    This will copy all PDFs from the lessons directory into the /pdf/
    directory under cw-website. The frontend Haskell program will then
    deploy these PDFs by simply copying them into _site.
    By using find instead of explicitly listing all the PDFs to copy, it's
    impossible to forget to add a PDF to the build script!

--- build.sh
+++ build.sh
@@ -3,10 +3,7 @@
 set -e
 
 make
-mv -vt cw-website/pdf \
-    lessons/0/plan/0-lp.pdf \
-    lessons/1/plan/1-lp.pdf \
-    lessons/syllabus/syllabus.pdf
+find lessons -name '*.pdf' -exec cp -v {} cw-website/pdf \;
 stack build
 (cd cw-website ; stack exec cw-website-exe rebuild)
 (cd cw-backend ; stack install)
