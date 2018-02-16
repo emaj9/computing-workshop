f564bea406541a7153f1031604b64d847162f848
Jacob Errington <git@mail.jerrington.me>
2018-02-15 23:42:04 -0500

    lesson 3: add list of functions students should try coding

---
+++ lessons/3/students.hs
@@ -0,0 +1,25 @@
+null :: [a] -> Bool
+null = _
+
+head :: [a] -> a
+head = _
+
+tail :: [a] -> [a]
+tail = _
+
+-- | Return the first integer in the list, plus one, if there is one.
+-- Otherwise return zero.
+plusOne :: [Int] -> Int
+plusOne = _
+
+-- | Add together the first two elements of the list.
+-- If there's only one element, just return it.
+-- If there's nothing, just return zero.
+addFirstTwo :: [Int] -> Int
+addFirstTwo = _
+
+length :: [a] -> Int
+length = _
+
+zip :: [a] -> [b] -> [(a, b)]
+zip = _
