ff2fbccece095aa8ed35326db9b6d7703da02e46
Jacob Errington <git@mail.jerrington.me>
2022-08-16 09:16:34 -0400

    s22 all for loop examples

---
+++ lessons/S22-CDSI/2/for-loops/loop1.py
@@ -0,0 +1,36 @@
+l = [1, 2, 3, 4, 5]
+a = 0
+for x in l:
+    a = a + x
+print(a)
+
+
+
+
+
+
+
+
+
+
+l1 = [1, 2, 3, 4, 5]
+l2 = []
+for x in l1:
+    l2.append(x * 2)
+print(l2)
+
+
+
+
+
+
+
+
+
+
+l = ["Jessica", "Damian", "Johnny", "Jennifer", "Bob", "Alice", "Aaron", "Leslie"]
+js = []
+for name in l:
+    if name[0] == "J":
+        js.append(x)
+print(js)
